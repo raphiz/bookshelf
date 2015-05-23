c96db6ed3493a9c20625ee81deed383814b0809d
raphi <mister.norbert@gmail.com>
2015-05-23 14:26:14 +0000

    get more recent poppler utils - from google drive

--- scripts/download_binaries.js
+++ scripts/download_binaries.js
@@ -1,4 +1,5 @@
-var http = require('http');
+var request = require('request');
+var https = require('https');
 var fs = require('fs');
 var unzip = require("unzip");
 var path = require("path");
@@ -11,46 +12,44 @@ if (typeof String.prototype.startsWith != 'function') {
 }
 
 
+
 function download(source, cb) {
-    var downloaded = path.basename(source),
-        file = fs.createWriteStream(downloaded);
-    console.log("Downloading....");
-    
-    http.get(source, function(response) {
-        response.pipe(file);
-        response.on('end', function() {
-            cb(downloaded);
-        });
-    }).on('error', function(e) {
-        console.log("Got error: " + e.message);
-    });
-
+  var downloaded = path.basename(source);
+  request({uri: source})
+      .pipe(fs.createWriteStream(downloaded))
+      .on('close', function() {
+        cb(downloaded);
+      });
 }
-
 
 var plattform = "32",
     destination = 'bin/';
 
 fs.mkdirSync(destination);
 
-// Download and "install" poppler utils
-download('http://gd.tuwien.ac.at/publishing/xpdf/xpdfbin-win-3.03.zip', function(downloaded){
-    fs.createReadStream(downloaded)
-      .pipe(unzip.Parse())
-      .on('entry', function (entry) {
-        var fileName = entry.path;
-        
-        // Extract all binaries into the bin directory
-        if (fileName.startsWith('xpdfbin-win-3.03/bin32/') && entry.type == "File") {
-            entry.pipe(fs.createWriteStream(destination  + fileName.split('/')[2]));
-        } else {
-          entry.autodrain();
-       }
-      })
-      .on('finish', function(){
-          console.log("cleaning up...")
-          fs.unlink(downloaded);
-      });
+
+// Binary originally provided by https://code.google.com/p/pdfbun/
+// Download and "install" pdfinfo
+download('http://goo.gl/Rfk6Tm', function(downloaded){
+    var source = fs.createReadStream(downloaded);
+    var dest = fs.createWriteStream('bin/pdfinfo.exe');
+    source.pipe(dest);
+    source.on('end', function() {
+        console.log("pdfinfo installed!")
+        fs.unlink(downloaded);
+    });
+});
+
+// Binary originally provided by https://code.google.com/p/pdfbun/
+// Download and "install" pdftoppm
+download('http://goo.gl/1uV3eh', function(downloaded){
+    var source = fs.createReadStream(downloaded);
+    var dest = fs.createWriteStream('bin/pdftoppm.exe');
+    source.pipe(dest);
+    source.on('end', function() {
+        console.log("pdftoppm installed!")
+        fs.unlink(downloaded);
+    });
 });
 
 // Download and "install" imagemagick
@@ -61,21 +60,16 @@ download('http://www.imagemagick.org/download/binaries/ImageMagick-6.9.1-2-Q16-x
         var fileName = entry.path;
         
         // Extract all binaries into the bin directory
-        
-//        if (fileName.startsWith('ImageMagick-6.9.1-2/images/') || fileName.startsWith('ImageMagick-6.9.1-2/www/')) {
         if(fileName == "ImageMagick-6.9.1-2/identify.exe" || fileName == "ImageMagick-6.9.1-2/convert.exe"){
-            console.log(fileName);
             entry.pipe(fs.createWriteStream(destination  + fileName.split('/')[1]));
         }else{
-    //     console.log(fileName); 
-        entry.autodrain();
+            entry.autodrain();
         }
           
        
       })
       .on('finish', function(){
-          console.log("cleaning up...")
           fs.unlink(downloaded);
+          console.log("ImageMagick installed")
       });
 });
-
